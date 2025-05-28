672cb289cae14bc78efec66d4f2b06c49afa0e53
guy32807 <guy32807@comcast.net>
2025-05-28 08:25:16 +0000

    Deploying to gh-pages from @ guy32807/card-connect@14bf1417522ce97ccd8c5f77c58b6833b0b6d0a3 🚀

---
+++ _next/static/chunks/app/error-9fae815e951ed0f1.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[601],{4002:function(e,r,n){Promise.resolve().then(n.bind(n,988))},988:function(e,r,n){"use strict";n.r(r),n.d(r,{default:function(){return Error}});var t=n(7437),o=n(2265);function Error(e){let{error:r,reset:n}=e;return(0,o.useEffect)(()=>{console.error(r)},[r]),(0,t.jsxs)("div",{className:"flex min-h-screen flex-col items-center justify-center p-6 text-center",children:[(0,t.jsx)("h2",{className:"text-2xl font-bold mb-4",children:"Something went wrong!"}),(0,t.jsx)("p",{className:"mb-6 text-gray-600",children:"We apologize for the inconvenience."}),(0,t.jsx)("button",{onClick:()=>n(),className:"px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700",children:"Try again"})]})}},622:function(e,r,n){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var t=n(2265),o=Symbol.for("react.element"),s=Symbol.for("react.fragment"),c=Object.prototype.hasOwnProperty,i=t.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,l={key:!0,ref:!0,__self:!0,__source:!0};function q(e,r,n){var t,s={},f=null,u=null;for(t in void 0!==n&&(f=""+n),void 0!==r.key&&(f=""+r.key),void 0!==r.ref&&(u=r.ref),r)c.call(r,t)&&!l.hasOwnProperty(t)&&(s[t]=r[t]);if(e&&e.defaultProps)for(t in r=e.defaultProps)void 0===s[t]&&(s[t]=r[t]);return{$$typeof:o,type:e,key:f,ref:u,props:s,_owner:i.current}}r.Fragment=s,r.jsx=q,r.jsxs=q},7437:function(e,r,n){"use strict";e.exports=n(622)}},function(e){e.O(0,[971,472,744],function(){return e(e.s=4002)}),_N_E=e.O()}]);
